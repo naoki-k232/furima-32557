99e1148b062a321ab55fee2a4f232db5e3f74dc9
naoki-k232 <74887536+naoki-k232@users.noreply.github.com>
2020-12-17 19:49:46 +0900

    商品出品機能単体テストコードまで

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,28 @@
 class Item < ApplicationRecord
 
+  with_options  presence: true do
+    validates     :title
+    validates     :produce_description
+    validates     :category_id
+    validates     :produce_condition_id
+    validates     :shipping_charges_id
+    validates     :prefecture_id
+    validates     :days_to_delivery_id
+    validates     :price, format: {with: /\A[0-9]+\z/, message:"半角数字でご入力ください。"}
+    validates     :image
+  end
+  
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
   belongs_to       :user
   has_one_attached :image
 
-  # 都道府県
-  belongs_to :prefecture
-  validates :prefecture, :text, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_to_delivery
+  belongs_to_active_hash :produce_condition
+  belongs_to_active_hash :shipping_charge
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+
+
+
+  describe '商品出品機能' do
+    
+
+    context '商品出品がうまくいくとき' do
+      it "画像と商品名と商品の説明とカテゴリーと商品の状態と配送料の負担と発送元の地域と発送までの日数と価格があれば出品できる" do
+      end
+      it "価格が、¥300~¥9,999,999の間であること"
+    end
+  end
 end
